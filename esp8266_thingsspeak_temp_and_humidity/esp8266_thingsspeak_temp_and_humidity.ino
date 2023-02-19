8ef2cbcc9c47cdd6e4be12af0ba4bebb08c048a5
=besufikad yilma <=07yilmabese@gmail.com>
2023-02-19 22:25:41 +0300

    sending random values to thingspeak for water, smoke and vibration sensor

--- esp8266_thingsspeak_temp_and_humidity/esp8266_thingsspeak_temp_and_humidity.ino
+++ esp8266_thingsspeak_temp_and_humidity/esp8266_thingsspeak_temp_and_humidity.ino
@@ -13,20 +13,37 @@ String myPWD = "lemlem0646"; // WiFi Password
 String myAPI = "FZQHE2NYUZZBJ5CO";   // API Key
 String myHOST = "api.thingspeak.com";
 String myPORT = "80";
+
+//thingspeak field names
 String myFIELD = "field1";
 String myFIELD2 = "field2";
+String myFIELD3 = "field3";
+String myFIELD4 = "field4";
+String myFIELD5 = "field5";
 
-int valSensor = 1;
+//dht sensor pin
 int tempsensor = 2;
+int vibsensor = 3;
+int MQsensor = A0;
+int watersensor = A1;
+// MQ135 gas_state = MQ135(MQsensor);
 
+
+
+
+//creat
 DHT dht(tempsensor , DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+
+  //start softwareserial for esp82600 communication
   espSerial.begin(115200);
 
+  //initialize dht sensor
   dht.begin();
 
+  //connect to wifi network
   espData("AT+RST", 1000, DEBUG);                      //Reset the ESP8266 module
   espData("AT+CWMODE=1", 1000, DEBUG);                 //Set the ESP mode as station mode
   espData("AT+CWJAP=\"" + mySSID + "\",\"" + myPWD + "\"", 1000, DEBUG);   //Connect to WiFi network
@@ -39,7 +56,7 @@ void setup() {
 }
 
 void loop() {
-  String getData = "GET /update?api_key=" + myAPI + "&" + myFIELD + "=" + getTemperatureValue() + "&" + myFIELD2 + "=" + getHumidityValue();
+  String getData = "GET /update?api_key=" + myAPI + "&" + myFIELD + "=" + getTemperatureValue() + "&" + myFIELD2 + "=" + getHumidityValue() + "&" + myFIELD3 + "=" + getVibrationValue() + "&" + myFIELD4 + "=" + getMqgasValue() + "&" + myFIELD5 + "=" + getwaterValue();
 
   espData("AT+CIPMUX=1", 1000, DEBUG);       //Allow multiple connections
   espData("AT+CIPSTART=0,\"TCP\",\"" + myHOST + "\"," + myPORT, 1000, DEBUG);
@@ -49,6 +66,7 @@ void loop() {
   delay(1000);
 }
 
+//returns temprature value read
 String getTemperatureValue() {
   float tempc = dht.readTemperature();
   Serial.print("Temperature(C) = ");
@@ -58,6 +76,7 @@ String getTemperatureValue() {
   return String(temp);
 }
 
+//returns temprature value read
 String getHumidityValue() {
   float humid = dht.readHumidity();
   Serial.print("Humidity in % = ");
@@ -66,6 +85,78 @@ String getHumidityValue() {
   delay(50);
   return String(humidity);
 }
+
+
+//get vibration value from vibration sensor
+String getVibrationValue(){
+
+// int vibration = analogRead(vibsensor);
+int vibration = random(10,100);
+// float mapper = map(vibration , 0 , 213 , 0 , 1);
+
+  // if (map == 1)
+  //         danger();
+  // else
+  //     normal();
+  //   mapper = 100*mapper;
+    Serial.print("percent of vibration:");
+    Serial.print(vibration);
+    Serial.print("\n");
+    delay(50);
+
+    return String(vibration);
+}
+
+
+//get value from smoke sensor
+String getMqgasValue(){
+  int ppm = random(100,1000);
+  // int ppm = gas_state.getPPM();
+  Serial.print("Rate:");
+  Serial.println(ppm);
+  Serial.print("PPM\n");
+  // if (ppm > 800)
+  //       danger();
+  // else 
+  //     normal();
+  //       delay(50);
+    return String(ppm);
+
+}
+
+//get value from water c sensor 
+String getwaterValue(){
+int water = random(10, 80);
+// int water = analogRead(watersensor);
+float mapper = map(water , 0 , 213 , 0 , 1);
+// if (map == 1)
+//   danger();
+//   else
+//   normal();
+//   mapper = 100*mapper;
+  Serial.print("Percentage Conductivity of Water:");
+  Serial.print(water);
+  Serial.print("\n");
+  delay(50);
+
+  return String(water); 
+}
+
+
+//danger and normal
+void danger(){
+
+  digitalWrite(red , HIGH);
+  digitalWrite(green , LOW);
+  digitalWrite(buzzer , HIGH);
+}
+void normal(){
+  digitalWrite(green , LOW);
+  digitalWrite(red , LOW);
+  digitalWrite(buzzer , HIGH);
+}
+
+
 
 String espData(String command, const int timeout, boolean debug) {
   Serial.print("AT Command ==> ");
